4d20c92b4c615fb4ef12f0aa46687bc872594fa8
Manugali <manugali@example.com>
2025-10-31 19:36:10 -0500

    Update Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Enable image optimization
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.githubusercontent.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
